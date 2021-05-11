d14857bba50aae18f61c7d41bf711e1efea6e7bd
tomomaru <tikksyou1129@gmail.com>
2021-05-11 15:13:51 +0900

    変更

--- organic/urls.py
+++ organic/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('index.urls')),
     path('profiles/',include('profiles.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
